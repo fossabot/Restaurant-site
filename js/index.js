fa2550a26ae58c2d98607e805be1a3a032d2a503
Syarol <1oleq25@gmail.com>
2017-04-21 22:27:54 +0300

    Add content and fix design
    Also add Font Awesome

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@
 $(function(){
         // Check the initial Poistion of the Sticky Header
         var stickyHeaderTop = $('.header').offset().top;
- 
+
         $(window).scroll(function(){
                 if( $(window).scrollTop() > stickyHeaderTop ) {
                         $('.header').css({position: 'fixed', top: '0px'});
