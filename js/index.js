f80ac42af0125c095e06330e67747ba3e6a53a50
Syarol <1oleq25@gmail.com>
2017-09-01 21:02:26 +0300

    Added highlighting of active menu item
    Also updated google map properties (disable sreet view, fullscreen and map type change)

--- js/index.js
+++ js/index.js
@@ -1,28 +1,30 @@
+function loadGoogleMapAPI(){
+  var script = document.createElement("script");
+  script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAEgnNcLXu3TxudcgyN9DnQ7uUwWy1hIpI&callback=loadMaps";
+  script.type = "text/javascript";
+  script.id ="googleMap";
+  document.getElementsByTagName("body")[0].append(script);
+}
 
-  function loadGoogleMapAPI(){
-    var script = document.createElement("script");
-    script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAEgnNcLXu3TxudcgyN9DnQ7uUwWy1hIpI&callback=loadMaps";
-    script.type = "text/javascript";
-    script.id ="googleMap";
-    document.getElementsByTagName("body")[0].append(script);
-  }
+window.loadMaps = function (){
+  var map = new google.maps.Map(document.getElementById('map_canvas'), {
+    center: {lat:50.745151, lng:25.322764},
+    zoom: 17,
+    streetViewControl: false,
+    mapTypeControl: false,
+    fullscreenControl: false
+  });
 
-  window.loadMaps = function (){
-    var map = new google.maps.Map(document.getElementById('map_canvas'), {
-      center: {lat: 50.744973, lng: 25.322932},
-      zoom: 17
-    });
+  var marker = new google.maps.Marker({
+    position: {lat:50.745151, lng:25.322764},
+    map: map,
+    title: 'Our Restaurant'
+  });
 
-    var marker = new google.maps.Marker({
-      position: {lat:50.745151, lng:25.322764},
-      map: map,
-      title: 'Our Restaurant'
-    });
-
-    google.maps.event.addListenerOnce(map, 'idle', function() {
-      google.maps.event.trigger(map, 'resize');
-    });
-  };
+  google.maps.event.addDomListener(map, 'idle', function() {
+    google.maps.event.trigger(map, 'resize');
+  });
+};
 
 function changeZIndex(IndexZ){
   var mapIndex = document.getElementsByClassName('map-wrapper'); 
@@ -31,78 +33,97 @@ function changeZIndex(IndexZ){
   }
 }
 
-  //map, that sliding from right side
-  $('#text-location').click(function() {
-    changeZIndex(11);
-    $('#map_canvas').css({marginLeft:'0', width:'100%'});
-
-    if(document.getElementById('googleMap')===null){
-      loadGoogleMapAPI();
-    };
-    $('.map-button').css({marginLeft: '2.5%'});
+//Highlighting  of active menu item
+function highlighting  (){ 
+  var scroll_top = $(document).scrollTop();
+  $("nav a").each(function(){
+    var target = $($(this).attr("href")); 
+    if (target.position().top - target.height() <= scroll_top && target.position().top - $('header').height() >= scroll_top) {
+      $("nav a.navigation-active").removeClass("navigation-active");
+      $(this).addClass("navigation-active");
+    } else {
+      $(this).removeClass("navigation-active");
+    }
   });
+}
 
-  //map, that sliding to right side
-  $('.map-return').click(function() {
-    $('#map_canvas').css({width: '0', marginLeft: '100%'});
-    setTimeout(function(){
-      changeZIndex(5);
-    },1700);
-    $('.fa-chevron-right').css({transform: 'rotate(180deg)'});
-    $('.map-button').css({marginLeft: '-10%'});
-    setTimeout(function(){
-       $('.fa-chevron-right').css({transform: 'rotate(0deg)'});
-    },1500);
-  });
+//map, that sliding from right side
+$('#text-location').click(function() {
+  changeZIndex(11);
+  $('#map_canvas').css({marginLeft:'0', width:'100%'});
+
+  if(document.getElementById('googleMap')===null){
+    loadGoogleMapAPI();
+  };
+  $('.map-button').css({marginLeft: '2.5%'});
+});
+
+//map, that sliding to right side
+$('.map-return').click(function() {
+  $('#map_canvas').css({width: '0', marginLeft: '100%'});
+  setTimeout(function(){
+    changeZIndex(5);
+  },1700);
+  $('.fa-chevron-right').css({transform: 'rotate(180deg)'});
+  $('.map-button').css({marginLeft: '-10%'});
+  setTimeout(function(){
+    $('.fa-chevron-right').css({transform: 'rotate(0deg)'});
+  },1500);
+});
 
 $(function(){
-  // Check the initial posistion of the Sticky Header
-  stickyHeaderTop = $('header').offset().top;
+  //Check the initial posistion of the Sticky Header
+  var stickyHeaderTop = $('header').offset().top;
   //If page open not top, then header must be top 
   if( $(window).scrollTop() > stickyHeaderTop ) {
     $('header').css({position: 'fixed', top: '0px'});
     $('.header-before-const').addClass('header-const');
   }
-  //Sticky Header when scrolling
+  
+  highlighting ();
+
   $(window).scroll(function(){
+    //Sticky Header when scrolling
     if( $(window).scrollTop() > stickyHeaderTop ) {
       $('header').css({position: 'fixed', top: '0px'});
       $('.header-before-const').addClass('header-const');
     } else {
       $('header').css({position: 'static'});
       $('.header-const').removeClass('header-const');
-    }
+    };
+
+    highlighting();
   });
 });
 
-  var menuF = $('.false-container');
-  var menuT = $('.small-menu-container > h1');
-  var menuC = $('.small-menu-container');
-  var titleM = $('.big-menu-container > h1');
+var menuF = $('.false-container');
+var menuT = $('.small-menu-container > h1');
+var menuC = $('.small-menu-container');
+var titleM = $('.big-menu-container > h1');
 
- function fadeMenuTypes (){
-    $(menuF).fadeToggle(1000, function(){
-      //setTimeout(2000,function(){$(this).detach()})
-    });
-    $(menuT).fadeToggle(1000, function(){
-      //setTimeout(2000,function(){$(this).detach()})
-    });
-    $(menuC).fadeToggle(1000, function(){
-      //setTimeout(2000,function(){$(this).detach()})
-    });
-    $(titleM).fadeToggle(1000, function(){
+function fadeMenuTypes (){
+  $(menuF).fadeToggle(1000, function(){
+    //setTimeout(2000,function(){$(this).detach()})
+  });
+  $(menuT).fadeToggle(1000, function(){
+    //setTimeout(2000,function(){$(this).detach()})
+  });
+  $(menuC).fadeToggle(1000, function(){
+    //setTimeout(2000,function(){$(this).detach()})
+  });
+  $(titleM).fadeToggle(1000, function(){
       
-    });
-  }  
-
-  menuF.click(function() {
-    fadeMenuTypes(); 
   });
-  menuT.click(function() {
-    fadeMenuTypes(); 
-  });
+}  
 
+menuF.click(function() {
+  fadeMenuTypes(); 
+});
+menuT.click(function() {
+  fadeMenuTypes(); 
+});
 
+//Lazy-load of images
 ;(function() {
   var bLazy = new Blazy({
     breakpoints: [
@@ -115,29 +136,24 @@ $(function(){
   });
 })();
 
-/*//smooth scrolling by sections
+//Smooth scrolling by sections
 $(function scrollScroll() {
   $.scrollify({
     section : ".big-size",
     easing : "swing",
     scrollSpeed : 1000,
     setHeights : false,
-    offset : -$(window).height()*0.1,
+    offset : -$('header').height(),
     overflowScroll: true,
     updateHash : true,
   });
+});
 
-  scrollScroll();
+//Smooth scrolling to all links
+$(".navigation").click(function() {
+  var hash = this.hash;// Store hash
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top-$('header').height()
+   }, 'slow', function(){});
+});
 
-  //Smooth scrolling to all links
-  $(".navigation").click(function() {
-    var hash = this.hash;// Store hash
-    $('html, body').animate({function(){
-      scrollTop: $(hash).offset().top-$('header').height();
-      $.scrollify({
-        standardScrollElements: "window"
-      });}
-    }, 1000, function(){});
-    scrollScroll();
-  });
-});*/
