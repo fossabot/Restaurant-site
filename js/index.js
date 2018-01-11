eb27c81e9796a0e599fb1c7137cf02dffee84b61
Syarol <1oleq25@gmail.com>
2018-01-11 23:05:26 +0200

    Added interactivity for menu
    Content will be in the future

--- js/index.js
+++ js/index.js
@@ -54,7 +54,7 @@ $('#text-location').click(function() {
 
   if(document.getElementById('googleMap')===null){
     loadGoogleMapAPI();
-  };
+  }
   $('.map-button').css({marginLeft: '2.5%'});
 });
 
@@ -91,36 +91,9 @@ $(window).scroll(function(){
     $('#header').css({position:'fixed',top: '0px'});
   } else {
     $('#header').css({position:'absolute',top: '90%'});
-  };
+  }
 
   highlighting();
-});
-
-var menuF = $('.false-container');
-var menuT = $('.small-menu-container > h1');
-var menuC = $('.small-menu-container');
-var titleM = $('.big-menu-container > h1');
-
-function fadeMenuTypes (){
-  $(menuF).fadeToggle(1000, function(){
-    //setTimeout(2000,function(){$(this).detach()})
-  });
-  $(menuT).fadeToggle(1000, function(){
-    //setTimeout(2000,function(){$(this).detach()})
-  });
-  $(menuC).fadeToggle(1000, function(){
-    //setTimeout(2000,function(){$(this).detach()})
-  });
-  $(titleM).fadeToggle(1000, function(){
-      
-  });
-}  
-
-menuF.click(function() {
-  fadeMenuTypes(); 
-});
-menuT.click(function() {
-  fadeMenuTypes(); 
 });
 
 //Lazy-load of images
@@ -179,9 +152,6 @@ $(".navigation").click(function() {
    }, 'slow', function(){});
 });
 
-
-
-
 var cloneMenuContainer, cloneContactContainer;
 
 var mediumMenuContainer = document.getElementById('medium-menu-container');
@@ -191,34 +161,19 @@ var menuContainer = document.getElementById('container-menu');
 var contactContainer = document.getElementById('container-contact');
 
 $(function(){
+  console.log(window.screen.orientation.type);
   cloneMenuContainer = menuContainer.cloneNode(true);
   cloneContactContainer = contactContainer.cloneNode(true);
-  if(window.screen.orientation.type == 'portrait-primary'){
-    var elMenu = document.getElementById('container-menu');
-    var elContact = document.getElementById('container-contact');
-
-    cloneMenu = elMenu.cloneNode();
-    cloneContact = elContact.cloneNode();
-
-    unwrapContainer(elMenu);
-    unwrapContainer(elContact);
+  console.log(window.innerWidth);
+  if(window.screen.orientation.type == 'portrait-primary' || window.innerWidth >= 1024){
+    saveNodeAndUnwrapContainers();
   }
 });
 
 window.addEventListener('orientationchange', function(){
   console.log(window.screen.orientation.type);
   if(window.screen.orientation.type == 'portrait-primary'){
-    var menuContainer = document.getElementById('container-menu');
-    var contactContainer = document.getElementById('container-contact');
-    
-    cloneMenuContainer = menuContainer.cloneNode(true);
-    cloneContactContainer = contactContainer.cloneNode(true);
-
-    var elMenu = document.getElementById('container-menu');
-    var elContact = document.getElementById('container-contact');
-
-    unwrapContainer(elMenu);
-    unwrapContainer(elContact);
+    saveNodeAndUnwrapContainers();
   } else {
     replaceToPrevious(mediumMenuContainer, cloneMenuContainer);
     replaceToPrevious(mediumContactContainer, cloneContactContainer);
@@ -237,4 +192,170 @@ function unwrapContainer(el){
 function  replaceToPrevious(parent, newNode){
   parent.innerHTML = '';//clear parent inner node
   parent.appendChild(newNode);//append old node instead empty place
+}
+
+function saveNodeAndUnwrapContainers (){
+  var menuContainer = document.getElementById('container-menu');
+  var contactContainer = document.getElementById('container-contact');
+    
+  cloneMenuContainer = menuContainer.cloneNode(true);
+  cloneContactContainer = contactContainer.cloneNode(true);
+
+  unwrapContainer(menuContainer);
+  unwrapContainer(contactContainer);
+}
+
+
+var backMenuButton = document.getElementById('menu-back-button');
+var menuPhotoContainer = document.getElementById('menu-dish-photo-container');
+var menuListContainer = document.getElementById('menu-dish-list-container');
+var menuMainTitle = document.getElementById('menu-main-title');
+var photoContainer = document.getElementById('menu-dish-photo');
+var dishIngredients = document.getElementById('menu-dish-ingredients');
+var menuDishList = document.getElementById('menu-dish-list');
+var menuListItem = document.getElementsByClassName('menu-dish-list-item');
+var containerMenu = document.getElementById('container-menu');
+var smallMenuContainer = document.getElementsByClassName('small-menu-container');
+var menuList; 
+var containerIndex; 
+var containerMenu = document.getElementById('container-menu');
+var smallEmptyMenuContainer = document.getElementsByClassName('empty-small-menu-container');
+
+var menuRequest = new XMLHttpRequest();
+menuRequest.open('GET', 'menu.json');
+menuRequest.responseType = 'text';
+menuRequest.send();
+
+menuRequest.addEventListener('load', function(e){
+  menuList = JSON.parse(menuRequest.response); // convert it to an object
+});
+
+menuDishList.onmouseover = function(e){
+  var target = e.target;
+  while (target != this) {
+    if (target.tagName == 'LI') {
+      var overIndex = getElementIndex(target);//index of mouseover li element
+      if (overIndex != -1) { 
+        showIngedientsPanel(overIndex);
+        changePhoto(overIndex);
+      }
+      return;
+    } else target = target.parentNode;
+  }
+};
+
+mediumMenuContainer.onclick = function(e){
+  var target = e.target;
+  while (target != this) {
+    if (target.classList.contains('small-menu-container')) {
+      containerIndex = getElementIndex(target);//get index of clicked element
+
+      containerMenu.style.display = 'none';
+      for (var i = 0; i < smallMenuContainer.length; i++) {
+        smallMenuContainer[i].style.display = 'none';
+      }
+
+      smallEmptyMenuContainer[0].style.display = 'none';
+
+      backMenuButton.style.display = 'inline-block';
+      menuPhotoContainer.style.display = 'flex';
+      menuListContainer.style.display = 'flex';
+      mediumMenuContainer.style.gridTemplateColumns = 'repeat(2, 1fr)';
+      mediumMenuContainer.style.gridAutoRows = '100%';
+
+
+
+      var targetChildren = target.children;
+      changeMenuMainTitle(targetChildren);
+
+      menuDishList.innerHTML = '';//clear list
+
+      for (i = 0; i < menuList.menu[containerIndex].length; i++){
+        createMenuPoint(i);
+      }
+
+      photoContainer.style.backgroundImage = target.style.backgroundImage;
+      photoContainer.style.backgroundPosition = target.style.backgroundPosition;
+
+      return;
+    } 
+    target = target.parentNode;
+    console.log(target);
+  }
+};
+
+backMenuButton.addEventListener('click', function(){
+  this.style.display = 'none';
+  menuMainTitle.textContent = 'Menu';
+  containerMenu.style.display = 'grid';
+  menuPhotoContainer.style.display = 'none';
+  menuListContainer.style.display = 'none';
+  console.log(window.innerWidth);
+  
+  for (var i = 0; i < smallMenuContainer.length; i++) {
+    smallMenuContainer[i].style.display = 'block';
+  }
+
+  dishIngredients.innerHTML = '';
+  dishIngredients.style.backgroundColor = 'rgba(182, 182, 182, 0)';
+
+  if(window.innerWidth <= '840'){
+    mediumMenuContainer.style.gridTemplateColumns = 'repeat(2, 1fr)';
+  } else if(window.innerWidth <= '1024' && window.innerWidth >= '600'){
+    mediumMenuContainer.style.gridTemplateColumns = '20% 60% 20%';
+    smallEmptyMenuContainer[0].style.display = 'block';
+  } else {
+    mediumMenuContainer.style.gridTemplateColumns = 'repeat(3, 1fr)';
+  }
+
+  mediumMenuContainer.style.gridAutoRows = 'auto';
+});
+
+function changeMenuMainTitle(targetChildren){
+  for (i = 0; i < targetChildren.length; i++) {
+    if(targetChildren[i].classList.contains('menu-titles')){
+      menuMainTitle.textContent = targetChildren[i].textContent;
+    }
+  }
+}
+
+function getElementIndex(node) {
+  var index = 0;
+  while ( (node = node.previousElementSibling) ) {
+    index++;
+  }
+  return index;
+}
+
+function showIngedientsPanel(overIndex){
+  var ingredients = document.createElement('p');
+  ingredients.textContent = menuList.menu[containerIndex][overIndex].ingredients;//get text from JSON
+
+  dishIngredients.innerHTML = '';
+  dishIngredients.appendChild(ingredients);
+  dishIngredients.style.backgroundColor = 'rgba(182, 182, 182, 0.9)';
+}
+
+function changePhoto(overIndex){
+  photoContainer.style.backgroundImage = 'url(' + menuList.menu[containerIndex][overIndex].image + ')';
+}
+
+/*Creates menu points and append them into list*/
+function createMenuPoint(i){
+  var dish = document.createElement('li');
+  var dishName = document.createElement('p');
+  var dishWeight = document.createElement('p');
+  var dishPrice = document.createElement('p');
+
+  dishName.textContent = menuList.menu[containerIndex][i].name;
+  dishWeight.textContent = menuList.menu[containerIndex][i].weight;
+  dishPrice.textContent = menuList.menu[containerIndex][i].price;
+
+  menuDishList.appendChild(dish);
+
+  dish.appendChild(dishName);
+  dish.appendChild(dishWeight);
+  dish.appendChild(dishPrice);
+
+  dish.setAttribute('class', 'menu-dish-list-item');
 }
